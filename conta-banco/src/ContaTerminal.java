1be7117ff1f77adba7267e984a90624ee76ce50e
carlinhoshk <carlosmdohk@gmail.com>
2024-07-30 16:00:25 -0300

    Feat: Finalizado projeto banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("teste");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        numero = sc.nextInt();
+        sc.nextLine();  // Consumir a linha restante
+
+        System.out.println("Por favor, digite o número da sua Conta: ");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o seu Nome: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite seu Saldo: ");
+        sc.hasNextDouble();
+        saldo = sc.nextDouble();
+        sc.nextLine();  // Consumir a linha restante
+
+
+
+        String mensagem = "Olá ".concat(nomeCliente)
+                .concat(", obrigado por criar uma conta em nosso banco. Sua agência é ")
+                .concat(agencia)
+                .concat(", conta número ")
+                .concat(String.valueOf(numero))
+                .concat(", e seu saldo de R$ ")
+                .concat(String.valueOf(saldo))
+                .concat(" já está disponível para saque.");
+
+
+        System.out.println(mensagem);
     }
 }
